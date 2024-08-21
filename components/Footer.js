f1a2a1e1ae0b95bb37af902a2813139b0f7975f4
priy-am <priyam2101753@gmail.com>
2024-08-21 09:23:21 +0530

    update2.0

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentyear = new Date().getFullYear()
   return (
     <footer className='bg-gray-900 px-2 h-14 items-center text-center text-white flex justify-center'>
-      <p> <span className='text-blue-700 mr-3'> copyright &copy; {currentyear} </span> GetMeChai - All rights reserved!</p>
+      <p> <span className='text-blue-700 mr-3'> copyright &copy; {currentyear} </span>  GetMeChai - All rights reserved!</p>
     </footer>
   )
 }
