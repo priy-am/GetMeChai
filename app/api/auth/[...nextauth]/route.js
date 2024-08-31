cc16cfb345cc042b2f6a92fa17e1f58f891647e4
priy-am <priyam2101753@gmail.com>
2024-08-31 13:35:55 +0530

    debug 2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -40,8 +40,6 @@ export const authoption = NextAuth({
     // }),
   ],
 
-  secret: process.env.NEXTAUTH_SECRET,
-
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       if (account.provider == "github") {
--- db/connectDb.js
+++ db/connectDb.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          })
+        const connect = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MonogDB connect :- ${connect.connection.host}`)
     } catch (error) {
         console.log(`Network error :- ${error}`)
